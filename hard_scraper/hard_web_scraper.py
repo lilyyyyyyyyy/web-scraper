dea9cddb8efbf248f91a175354768ed4e41596b4
Lily <lily@>
2017-07-29 22:36:51 -0400

    get the raw contents of the givin website

--- hard_scraper/hard_web_scraper.py
+++ hard_scraper/hard_web_scraper.py
@@ -1,1 +1,7 @@
 
+import requests
+from bs4 import BeautifulSoup
+path = "http://forecast.weather.gov/MapClick.php?lat=32.7157&lon=-117.1617"
+page = requests.get(path)
+soup = BeautifulSoup(page.content, 'html.parser')
+print(soup)
