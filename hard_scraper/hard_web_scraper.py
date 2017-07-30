2a73e5f54b51dba4584e88bf98bb135c39e2cdca
Lily <lily@>
2017-07-29 22:41:09 -0400

    locate the content with id = seven-day-forecast

--- hard_scraper/hard_web_scraper.py
+++ hard_scraper/hard_web_scraper.py
@@ -4,4 +4,7 @@ from bs4 import BeautifulSoup
 path = "http://forecast.weather.gov/MapClick.php?lat=32.7157&lon=-117.1617"
 page = requests.get(path)
 soup = BeautifulSoup(page.content, 'html.parser')
-print(soup)
+
+seven_day = soup.find(id="seven-day-forecast")
+
+print(seven_day.prettify())
