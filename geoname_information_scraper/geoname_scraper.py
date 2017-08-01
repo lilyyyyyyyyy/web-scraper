7c9007b72557be1b5b62dd1b8937fe6df7ddc406
Lily <lily@>
2017-08-01 09:58:51 -0400

    getting the ZIP code, name and longitude and latitude of cities or counties in NY

--- geoname_information_scraper/geoname_scraper.py
+++ geoname_information_scraper/geoname_scraper.py
@@ -4,3 +4,13 @@ from bs4 import BeautifulSoup
 page_NY = requests.get("http://www.geonames.org/postal-codes/US/NY/new-york.html")
 soup = BeautifulSoup(page_NY.content, 'html.parser')
 print(soup)
+
+city_NY = soup.find(class_="restable")
+
+lon_and_lat_NY = [ll.get_text() for ll in city_NY.select("a small")]
+zipcode_NY = [zp.get_text() for zp in city_NY.select("tr td")][2::9]
+city_name_NY = [city.get_text() for city in city_NY.select("tr td")][1::9]
+
+print(lon_and_lat_NY)
+print(zipcode_NY)
+print(city_name_NY)
