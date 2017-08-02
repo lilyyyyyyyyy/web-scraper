b4e68f3f49a5c7e5169bcad2d6e52cc8fb4333d4
Lily <lily@>
2017-08-02 18:52:24 -0400

    get the weather forecast by hard code through API

--- API-version/API_web_scraper.py
+++ API-version/API_web_scraper.py
@@ -14,4 +14,20 @@ lon = [l.get_text() for l in soup1.select("lng")][0]
 
 url = "https://api.weather.gov/points/{0},{1}/forecast" .format(lat, lon)
 
-print(url)
+response2 = requests.get(url)
+data = response2.json()
+data_extract = data["properties"]["periods"]
+
+period_tags = [x['name'] for x in data_extract]
+short_descs = [x['shortForecast'] for x in data_extract]
+descs = [x['detailedForecast'] for x in data_extract]
+temp = [x['temperature'] for x in data_extract]
+
+weather = pd.DataFrame({
+        "period": period_tags,
+        "short_desc": short_descs,
+        "temp": temp,
+        "desc":descs
+    })
+
+print(weather)
