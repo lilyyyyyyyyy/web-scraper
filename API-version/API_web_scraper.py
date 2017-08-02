c5b83992002972238128ef8ec9ab2d551bfa8e21
Lily <lily@>
2017-08-02 18:50:59 -0400

    get the url through API

--- API-version/API_web_scraper.py
+++ API-version/API_web_scraper.py
@@ -9,4 +9,9 @@ parameters1 = {"postalcode": 10003, "country": "US", "username":  "emililyyyyyyy
 response1 = requests.get(path, params = parameters1 )
 soup1 = BeautifulSoup(response1.content, 'html.parser')
 
-print(soup1)
+lat = [la.get_text() for la in soup1.select("lat")][0]
+lon = [l.get_text() for l in soup1.select("lng")][0]
+
+url = "https://api.weather.gov/points/{0},{1}/forecast" .format(lat, lon)
+
+print(url)
