55ee7a7f2781d61dcc21bfb63271342d65ff8b70
Lily <lily@>
2017-08-01 21:07:01 -0400

    combine

--- hard_scraper/hard_web_scraper.py
+++ hard_scraper/hard_web_scraper.py
@@ -1,11 +1,37 @@
+
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import csv
 
+acronym_of_state_name = input("Please enter the acronym of state you are interested in: ")
 
-path = "http://forecast.weather.gov/MapClick.php?lat=32.7157&lon=-117.1617"
-page = requests.get(path)
+page = requests.get("http://www.geonames.org/postalcode-search.html?q=&country=US&adminCode1={0}".format(acronym_of_state_name))
+soup = BeautifulSoup(page.content, 'html.parser')
+
+city = soup.find(class_="restable")
+
+lon_and_lat = [ll.get_text() for ll in city.select("a small")]
+zipcode = [zp.get_text() for zp in city.select("tr td")][2::9]
+city_name = [city.get_text() for city in city.select("tr td")][1::9]
+
+STATE = pd.DataFrame({
+            "city in NY": city_name,
+            "zip code": zipcode,
+            "longitude and latitude": lon_and_lat})
+
+# STATE.to_csv('NY.csv', index=False)
+print(STATE)
+
+identifier = input("Please enter the ZIP code: " )
+
+lon_lat = STATE.loc[STATE['zip code'] == identifier]
+lon = list(lon_lat["longitude and latitude"])[0]
+a, b = lon.split("/")
+
+url = "http://forecast.weather.gov/MapClick.php?lat={0}&lon={1}".format(a,b)
+
+page = requests.get(url)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 seven_day = soup.find(id="seven-day-forecast")
